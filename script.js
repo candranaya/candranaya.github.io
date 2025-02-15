337e7d25c9e4fef4c983754dfa625cd6cc0a396b
Adang Irawan <adangirawan00@gmail.com>
2025-02-15 16:26:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.remove('active');
+    });
+});
